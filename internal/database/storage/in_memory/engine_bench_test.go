38589878827fa37b671f489616cebe477dc726e8
Zakariya <pzakariya@mail.ru>
2026-06-29 17:51:46 +0500

    completely add tests bench with stats

--- internal/database/storage/in_memory/engine_bench_test.go
+++ internal/database/storage/in_memory/engine_bench_test.go
@@ -1,6 +1,7 @@
 package inmemory
 
 import (
+	"fmt"
 	"math/rand"
 	"strconv"
 	"testing"
@@ -8,7 +9,7 @@ import (
 
 var sink string
 
-func benchMixedParallel(b *testing.B, get func(string) (string, bool), set func(string, string)) {
+func benchMixedParallel(b *testing.B, get func(string) (string, bool), set func(string, string), writePct int) {
 	const keyspace = 100_000
 	for i := 0; i < keyspace; i++ {
 		set("key"+strconv.Itoa(i), "value")
@@ -19,10 +20,10 @@ func benchMixedParallel(b *testing.B, get func(string) (string, bool), set func(
 		rng := rand.New(rand.NewSource(rand.Int63()))
 		for pb.Next() {
 			k := "key" + strconv.Itoa(rng.Intn(keyspace))
-			if rng.Intn(100) < 90 {
-				sink, _ = get(k)
-			} else {
+			if rng.Intn(100) < writePct {
 				set(k, "value")
+			} else {
+				sink, _ = get(k)
 			}
 		}
 	})
@@ -30,10 +31,43 @@ func benchMixedParallel(b *testing.B, get func(string) (string, bool), set func(
 
 func BenchmarkSingle_Mixed_Parallel(b *testing.B) {
 	e := NewSingleMapEngine()
-	benchMixedParallel(b, e.Get, e.Set)
+	benchMixedParallel(b, e.Get, e.Set, 10)
 }
 
 func BenchmarkPartitioned_Mixed_Parallel(b *testing.B) {
 	e := NewHashBasedPartitionMapEngine()
-	benchMixedParallel(b, e.Get, e.Set)
+	benchMixedParallel(b, e.Get, e.Set, 10)
+}
+
+func BenchmarkPartitioned_Mixed(b *testing.B) {
+	for _, w := range []int{1, 10, 50} {
+		b.Run(fmt.Sprintf("writes=%d%%", w), func(b *testing.B) {
+			e := NewHashBasedPartitionMapEngine()
+			benchMixedParallel(b, e.Get, e.Set, w)
+		})
+	}
+}
+
+func BenchmarkMixed_ByWritePct(b *testing.B) {
+	engines := []struct {
+		name string
+		new  func() (get func(string) (string, bool), set func(string, string))
+	}{
+		{"single", func() (func(string) (string, bool), func(string, string)) {
+			e := NewSingleMapEngine()
+			return e.Get, e.Set
+		}},
+		{"partitioned", func() (func(string) (string, bool), func(string, string)) {
+			e := NewHashBasedPartitionMapEngine()
+			return e.Get, e.Set
+		}},
+	}
+	for _, w := range []int{1, 10, 50} {
+		for _, eng := range engines {
+			b.Run(fmt.Sprintf("%s/writes=%d%%", eng.name, w), func(b *testing.B) {
+				get, set := eng.new()
+				benchMixedParallel(b, get, set, w)
+			})
+		}
+	}
 }
