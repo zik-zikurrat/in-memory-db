d836b5fc85d847d978c4e3df74bd236e4a350585
Zakariya <pzakariya@mail.ru>
2026-06-29 17:32:15 +0500

    some refactor

--- internal/database/storage/in_memory/engine_bench_test.go
+++ internal/database/storage/in_memory/engine_bench_test.go
@@ -1,6 +1,7 @@
 package inmemory
 
 import (
+	"math/rand"
 	"strconv"
 	"testing"
 )
@@ -14,4 +15,30 @@ func BenchmarkSingleMapEngine_Set(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		engine.Set("key"+strconv.Itoa(i), "value")
 	}
+}
+
+var sink string
+
+func BenchmarkEngine_Mixed_Parallel(b *testing.B) {
+	e := NewHashBasedPartitionMapEngine()
+
+	const keyspace = 100_000
+	for i := 0; i < keyspace; i++ {
+		e.Set("key"+strconv.Itoa(i), "value")
+	}
+
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	b.RunParallel(func(pb *testing.PB) {
+		rng := rand.New(rand.NewSource(rand.Int63()))
+		for pb.Next() {
+			k := "key" + strconv.Itoa(rng.Intn(keyspace))
+			if rng.Intn(100) < 90 {
+				sink, _ = e.Get(k)
+			} else {
+				e.Set(k, "value")
+			}
+		}
+	})
 }
