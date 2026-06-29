451ffd82d093309a979a234a09fe27d4ed47e129
Zakariya <pzakariya@mail.ru>
2026-06-29 17:43:12 +0500

    compare digits in tests

--- internal/database/storage/in_memory/engine_bench_test.go
+++ internal/database/storage/in_memory/engine_bench_test.go
@@ -6,39 +6,34 @@ import (
 	"testing"
 )
 
-func BenchmarkSingleMapEngine_Set(b *testing.B) {
-	engine := NewSingleMapEngine()
-
-	b.ReportAllocs()
-	b.ResetTimer()
-
-	for i := 0; i < b.N; i++ {
-		engine.Set("key"+strconv.Itoa(i), "value")
-	}
-}
-
 var sink string
 
-func BenchmarkEngine_Mixed_Parallel(b *testing.B) {
-	e := NewHashBasedPartitionMapEngine()
-
+func benchMixedParallel(b *testing.B, get func(string) (string, bool), set func(string, string)) {
 	const keyspace = 100_000
 	for i := 0; i < keyspace; i++ {
-		e.Set("key"+strconv.Itoa(i), "value")
+		set("key"+strconv.Itoa(i), "value")
 	}
-
 	b.ReportAllocs()
 	b.ResetTimer()
-
 	b.RunParallel(func(pb *testing.PB) {
 		rng := rand.New(rand.NewSource(rand.Int63()))
 		for pb.Next() {
 			k := "key" + strconv.Itoa(rng.Intn(keyspace))
 			if rng.Intn(100) < 90 {
-				sink, _ = e.Get(k)
+				sink, _ = get(k)
 			} else {
-				e.Set(k, "value")
+				set(k, "value")
 			}
 		}
 	})
+}
+
+func BenchmarkSingle_Mixed_Parallel(b *testing.B) {
+	e := NewSingleMapEngine()
+	benchMixedParallel(b, e.Get, e.Set)
+}
+
+func BenchmarkPartitioned_Mixed_Parallel(b *testing.B) {
+	e := NewHashBasedPartitionMapEngine()
+	benchMixedParallel(b, e.Get, e.Set)
 }
