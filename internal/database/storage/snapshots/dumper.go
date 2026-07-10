6c0e00fab06a65dedd1b26bdcdaf64846afa813a
Zakariya <pzakariya@mail.ru>
2026-07-10 17:40:38 +0500

    tmp file for dump.json and remove rwmutex from dump

--- internal/database/storage/snapshots/dumper.go
+++ internal/database/storage/snapshots/dumper.go
@@ -14,7 +14,6 @@ type Snapshotable interface {
 }
 
 type HashBasedPartitionDumper struct {
-	mu       sync.Mutex
 	engine   *inmemory.HashBasedPartitionMapEngine
 	dumpDir  string
 	dumpFile string
@@ -34,13 +33,12 @@ func (d *HashBasedPartitionDumper) Dump() error {
 	dump := make([]map[string]string, 0, len(buckets))
 	wg := sync.WaitGroup{}
 	wg.Add(workers)
-	for _, partition := range buckets {
-		go func(partition *inmemory.Partition) {
+
+	for i, partition := range buckets {
+		go func(i int, p *inmemory.Partition) {
 			defer wg.Done()
-			d.mu.Lock()
-			defer d.mu.Unlock()
-			dump = append(dump, partition.Snapshot())
-		}(partition)
+			dump[i] = p.Snapshot()
+		}(i, partition)
 	}
 	wg.Wait()
 
@@ -52,25 +50,34 @@ func (d *HashBasedPartitionDumper) Dump() error {
 }
 
 func (d *HashBasedPartitionDumper) writeDump(dump []map[string]string) error {
-	// create or open exists file
-	f, err := os.OpenFile(filepath.Join(d.dumpDir, d.dumpFile), os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0o644)
+	tmpPath := filepath.Join(d.dumpDir, d.dumpFile+".tmp")
+	finalPath := filepath.Join(d.dumpDir, d.dumpFile)
+
+	data, err := json.Marshal(dump)
 	if err != nil {
 		return err
 	}
 
-	// convert dump to byte slice
-	byteDump, err := json.Marshal(dump)
+	f, err := os.OpenFile(tmpPath, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0o644)
 	if err != nil {
 		return err
 	}
 
-	// write dump
-	_, err = f.Write(byteDump)
-	if err != nil {
+	if _, err := f.Write(data); err != nil {
+		f.Close()
 		return err
 	}
 
-	return nil
+	if err := f.Sync(); err != nil {
+		f.Close()
+		return err
+	}
+
+	if err := f.Close(); err != nil {
+		return err
+	}
+
+	return os.Rename(tmpPath, finalPath)
 }
 
 func (d *HashBasedPartitionDumper) Load() error {
